e4b280480f78a01807bd3c190d64e00fe1da59df
martbungque <raymartbungque18@gmail.com>
2025-08-26 17:30:32 +0800

    Corrected the endpoint for the api route by adding /api

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 // Routes
-app.use("/users", userRoutes);
-app.use("/tasks", taskRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/tasks", taskRoutes);
 
-
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
