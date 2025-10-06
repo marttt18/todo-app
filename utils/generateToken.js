25eff9d7ed8947787e4180dc64a83e05dd1f296e
martbungque <raymartbungque18@gmail.com>
2025-10-07 01:23:47 +0800

    Changed the payload parameter to userId

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,10 +1,11 @@
 import jwt from 'jsonwebtoken';
 
-const generateToken = (payload) => {
-    return jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '1hr' });
+const generateToken = (userId) => {
+    return jwt.sign(
+        { userId },
+        process.env.JWT_SECRET_KEY,
+        { expiresIn: '1hr' }
+    );
 }
-// Q: is the payload must be an object?
-// A: Yes, the payload must be an object. It can contain any information you want to include in the token, such as user ID, email, roles, etc.
-// Q: Since we have only one parameter which is the payload, and if we want to pass not jjust the id but also the email, how do we do that?
-// A: We can pass an object as the payload. For example: generateToken({ id: user._id, email: user.email });
+
 export default generateToken;
