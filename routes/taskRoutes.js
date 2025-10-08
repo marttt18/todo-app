82d868c021d3da9d40a8f83bf71f3f59e8fce09f
martbungque <raymartbungque18@gmail.com>
2025-10-08 23:01:19 +0800

    added a get a task by id route

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,9 +1,8 @@
-// Routes for handling tasks (private - requires authentication)
-
 import express from 'express';
 import { 
     getTasks, 
-    createTask, 
+    createTask,
+    getTaskById, 
     updateTask, 
     deleteAllTasks, 
     deleteTask 
@@ -12,17 +11,18 @@ import { validateToken } from '../middleware/validateToken.js';
 
 const router = express.Router();
 
-// /api/tasks
+//@route /api/tasks
+//@access private
 router.route('/')
-    .get(getTasks)       // Display all tasks
+    .get(validateToken, getTasks)       // Display all tasks
     .post(validateToken, createTask)   // Add a task
     .delete(validateToken, deleteAllTasks); // Delete all tasks
 
-// /api/tasks/:id
+//@route /api/tasks/:id
+//@access private
 router.route('/:id')
+    .get(validateToken, getTaskById)    // Get a task by id
     .put(validateToken, updateTask)     // Update a task by id
     .delete(validateToken, deleteTask); // Delete a task by id
-
-    // Added made: added the validateToken middleware to the routes that require authentication
 
 export default router;
