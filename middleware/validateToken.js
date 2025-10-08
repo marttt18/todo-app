a8a6926fe83df21a0e4eca2a6316f9818ec7a431
martbungque <raymartbungque18@gmail.com>
2025-10-08 23:02:42 +0800

    changed the status code values from actual values to STATUS_CODES from constants.js

--- middleware/validateToken.js
+++ middleware/validateToken.js
@@ -1,12 +1,12 @@
 import jwt from 'jsonwebtoken';
-import { constants } from '../constants.js';
+import { STATUS_CODES } from '../constants.js';
 
 export const validateToken = (req, res, next) => {
     const authHeader = req.headers.authorization || req.headers.Authorization;
 
     // Check if the authorization header is present and starts with 'Bearer '
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        res.status(constants.UNAUTHORIZED);
+        res.status(STATUS_CODES.UNAUTHORIZED);
         throw new Error('Unauthorized: No token provided');
     }
 
@@ -14,19 +14,19 @@ export const validateToken = (req, res, next) => {
     const token = authHeader.split(' ')[1]; 
 
     if (!token) {
-        res.status(constants.UNAUTHORIZED);
+        res.status(STATUS_CODES.UNAUTHORIZED);
         throw new Error('Unauthorized: No token provided');
     }
 
     // Validate the token
     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
         if (err) {
-            res.status(constants.FORBIDDEN);
+            res.status(STATUS_CODES.FORBIDDEN);
             throw new Error('Forbidden: Invalid token');
         }
-        req.user = decoded // i only passed the user id in the payload. what will be the value of the payload?
+        req.user = decoded
         console.log("Decoded token payload: ", decoded);
 
-        next(); // Proceed to the next middleware or route handler
+        next();
     });
 }
