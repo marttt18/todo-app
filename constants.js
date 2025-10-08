5aa02a7b3b9f78b68221bb8575eeec14a7a923ba
martbungque <raymartbungque18@gmail.com>
2025-10-08 23:00:18 +0800

    added new status code values and rename it from constants -> STATUS_CODES

--- constants.js
+++ constants.js
@@ -1,7 +1,9 @@
-export const constants = {
+export const STATUS_CODES = {
+    OK: 200,
+    CREATED: 201,
     VALIDATION_ERROR: 400,
     UNAUTHORIZED: 401,
     FORBIDDEN: 403,
     NOT_FOUND: 404,
-    SERVER_ERROR: 500
-};
+    INTERNAL_SERVER_ERROR: 500
+  };
